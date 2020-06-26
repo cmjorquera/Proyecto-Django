458348fe4cc0230f7eee3c5de6c272cad8887582
Cristian Jorquera <cm.jorquerag@gmail.com>
2020-06-26 00:06:58 -0400

    modificacion

--- jorqueraWeb/urls.py
+++ jorqueraWeb/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include     # include para incluir las urls de APP
+from django.urls import path
+from django.urls import include     # include para incluir las urls de APP
 
 
 ##################### IMPORTACIONES IMPORTANTE #####################
